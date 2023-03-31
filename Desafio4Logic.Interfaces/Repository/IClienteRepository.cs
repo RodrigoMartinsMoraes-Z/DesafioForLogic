d052344d8d71ba876e2f9f0b15f0c0b76cbadf2c
Rodrigo Martins Moraes <rodrigo_moraes@hotmail.com.br>
2023-03-31 18:35:39 -0300

    cliente repository

---
+++ Desafio4Logic.Interfaces/Repository/IClienteRepository.cs
@@ -0,0 +1,17 @@
+﻿using Desafio4Logic.Domain.Clientes;
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Desafio4Logic.Interfaces.Repository
+{
+    public interface IClienteRepository
+    {
+        Task AtualizarCliente(Cliente cliente);
+        Task<Cliente> BuscarClientePorCNPJ(string cnpj);
+        Task<Cliente> BuscarClientePorNome(string nome);
+        Task<Cliente> SalvarCliente(Cliente cliente);
+    }
+}
---
+++ Desafio4Logic.Repository/Clientes/ClienteRepository.cs
@@ -0,0 +1,46 @@
+﻿using Desafio4Logic.Domain.Clientes;
+using Desafio4Logic.Interfaces.Context;
+using Desafio4Logic.Interfaces.Repository;
+
+using Microsoft.EntityFrameworkCore;
+
+using System.Threading.Tasks;
+
+namespace Desafio4Logic.Repository.Clientes
+{
+
+
+    public class ClienteRepository : IClienteRepository
+    {
+        private readonly ISQLContext _sqlContext;
+
+        public ClienteRepository(ISQLContext sqlContext)
+        {
+            _sqlContext = sqlContext;
+        }
+
+        public async Task<Cliente> BuscarClientePorNome(string nome)
+        {
+            return await _sqlContext.Clientes.FirstOrDefaultAsync(c => c.Nome == nome);
+        }
+
+        public async Task<Cliente> BuscarClientePorCNPJ(string cnpj)
+        {
+            return await _sqlContext.Clientes.FirstOrDefaultAsync(c => c.CNPJ == cnpj);
+        }
+
+        public async Task<Cliente> SalvarCliente(Cliente cliente)
+        {
+            await _sqlContext.Clientes.AddAsync(cliente);
+            await _sqlContext.SaveChangesAsync();
+
+            return cliente;
+        }
+
+        public async Task AtualizarCliente(Cliente cliente)
+        {
+            _sqlContext.Clientes.Update(cliente);
+            await _sqlContext.SaveChangesAsync();
+        }
+    }
+}
