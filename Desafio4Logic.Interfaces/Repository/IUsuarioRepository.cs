a703c257ad96ceb15f3039382beb06ad998b99d2
Rodrigo Martins Moraes <rodrigo_moraes@hotmail.com.br>
2023-03-31 17:55:24 -0300

    repositorio usuario criado

---
+++ Desafio4Logic.Interfaces/Repository/IUsuarioRepository.cs
@@ -0,0 +1,13 @@
+﻿using Desafio4Logic.Domain.Usuarios;
+
+using System.Threading.Tasks;
+
+namespace Desafio4Logic.Interfaces.Repository
+{
+    public interface IUsuarioRepository
+    {
+        Task AtualizarUsuario(Usuario usuario);
+        Task<Usuario> BuscarUsuarioPorId(int id);
+        Task<Usuario> SalvarUsuario(Usuario usuario);
+    }
+}
--- Desafio4Logic.Repository/Usuarios/UsuarioRepository.cs
+++ Desafio4Logic.Repository/Usuarios/UsuarioRepository.cs
@@ -1,10 +1,43 @@
-﻿using System;
+﻿using Desafio4Logic.Domain.Usuarios;
+using Desafio4Logic.Interfaces.Context;
+using Desafio4Logic.Interfaces.Repository;
+
+using System;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace Desafio4Logic.Repository.Usuarios
 {
-    internal class UsuarioRepository
+
+
+    public class UsuarioRepository : IUsuarioRepository
     {
+        private readonly ISQLContext _sqlContext;
+
+        public UsuarioRepository(ISQLContext sqlContext)
+        {
+            _sqlContext = sqlContext;
+        }
+
+        public async Task<Usuario> SalvarUsuario(Usuario usuario)
+        {
+            await _sqlContext.Usuarios.AddAsync(usuario);
+            await _sqlContext.SaveChangesAsync();
+            return usuario;
+        }
+
+        public async Task<Usuario> BuscarUsuarioPorId(int id)
+        {
+            return await _sqlContext.Usuarios.FindAsync(id);
+        }
+
+        public async Task AtualizarUsuario(Usuario usuario)
+        {
+            _sqlContext.Usuarios.Update(usuario);
+            await _sqlContext.SaveChangesAsync();
+        }
+
     }
 }
