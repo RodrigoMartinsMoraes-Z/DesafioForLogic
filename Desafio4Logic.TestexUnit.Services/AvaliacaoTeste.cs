075fb46e0684751bef97215e8b236ef00b41231e
Rodrigo Martins Moraes <rodrigo_moraes@hotmail.com.br>
2023-04-02 11:27:00 -0300

    sonar adicionado

---
+++ Desafio4Logic.TestexUnit.Services/AvaliacaoTeste.cs
@@ -0,0 +1,17 @@
+using Desafio4Logic.Context;
+
+using System;
+
+using Xunit;
+
+namespace Desafio4Logic.TestexUnit.Services
+{
+    public class AvaliacaoTeste
+    {
+        [Fact]
+        public void CadastrarAvaliacaoComSucesso()
+        {
+            var contexto = new SQLContext();
+        }
+    }
+}
