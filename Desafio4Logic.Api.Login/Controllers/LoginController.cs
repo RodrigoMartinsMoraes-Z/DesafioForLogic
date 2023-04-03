db7c343e3a224c7bf9b1ca58c89e5ba720799188
Rodrigo Martins Moraes <rodrigo_moraes@hotmail.com.br>
2023-04-03 15:01:23 -0300

    login controller

---
+++ Desafio4Logic.Api.Login/Controllers/LoginController.cs
@@ -0,0 +1,30 @@
+﻿using Desafio4Logic.Interfaces.Services;
+using Desafio4Logic.Models;
+using Desafio4Logic.Models.Usuarios;
+
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+using System.Threading.Tasks;
+
+namespace Desafio4Logic.Api.Login.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoginController : ControllerBase
+    {
+        private readonly ILoginService _loginService;
+
+        public LoginController(ILoginService loginService)
+        {
+            _loginService = loginService;
+        }
+
+        [HttpPost]
+        [Route("realizar-login")]
+        public async Task<RespostaPadrao> RealizarLogin(UsuarioModel usuarioModel)
+        {
+            return await _loginService.RealizarLogin(usuarioModel);
+        }
+    }
+}
